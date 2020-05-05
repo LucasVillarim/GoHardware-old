cc9c6716d1be42f67db29e54bf0d3f9366615234
LucasVillarim <villarim94@gmail.com>
2020-05-05 20:42:12 -0300

    css progress

--- frontend/src/Pages/Profile/index.js
+++ frontend/src/Pages/Profile/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './style.css';
 
@@ -8,14 +9,16 @@ export default function Profile() {
 
             <div className = 'container-box-left'>
                 <h1>Menu</h1>
-                <button>Button 1</button>
-                <button>Button 1</button>
-                <button>Button 1</button>
-                <button>Button 1</button>
-                <button>Button 1</button>
+                <Link to = "/profile/saved">Saved</Link>
+                <Link to = "">Compare</Link>
+                <Link to = "">Store</Link>
+                <Link to = "">Reviews</Link>
+                <Link to = "">Button 1</Link>
             </div>
 
             <div className = 'container-box-right'>
+            <div className = "glass-background2"></div>
+
                 <h1>Control Panel</h1>
 
                 <div className = "last-comparison-box">
@@ -23,20 +26,23 @@ export default function Profile() {
                     <label>Hardware X</label>
                     <label>Hardware Y</label>
 
+                    <div className = "glass-background1"></div>
                     <div className = "glass-detail1"></div>
                     <div className = "glass-detail2"></div>
                 </div>
 
                 <div className = "hardware-data-box">
-                    <label>Specifications</label>
-                    <label>Brand Name</label>
-                    <label>Hardware X</label>
-                    <label>VRAM</label>
-                    <label>Clock</label>
-                    <label>Power demand</label>
+                    <label>Specifications : Lorem ipsum</label>
+                    <label>Brand Name : Lorem ipsum</label>
+                    <label>Hardware X : Lorem ipsum</label>
+                    <label>VRAM : Lorem ipsum</label>
+                    <label>Clock : Lorem ipsum</label>
+                    <label>Power demand : 000W</label>
+                
+                    
+                    <div className = "glass-detail1-box2"></div>
                 </div>
             </div>
-
         </div>
     );
 }
