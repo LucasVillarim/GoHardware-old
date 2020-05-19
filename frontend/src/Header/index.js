8242d7cf312da576e09a4d291d8dc5da132035c6
LucasVillarim <villarim94@gmail.com>
2020-05-19 00:27:28 -0300

    header fix

--- frontend/src/Header/index.js
+++ frontend/src/Header/index.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { FiPower } from 'react-icons/fi'
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import './style.css';
 
 export default function Header() {
 
+    const history = useHistory();
+    const token = localStorage.getItem('jwt');
+
+    async function handleLogout() {
+        localStorage.clear();
+        history.push('/');
+    }
+    
     return(
         <div className = "header">
             <Link to = "/" className = "main-logo">GoHardware</Link>
 
             <Link to = "/sobre" className = "sobre">Sobre</Link>
 
-            <Link to = "/login" className = "login"><strong>LogIn</strong></Link>
-            <button className = "exit"><FiPower size = {26} color = "#c4c4c4"/></button>
+            { token ? "" : <Link to = "/login" className = "login"><strong>LogIn</strong></Link> }
+            { token ? <button 
+            className = "exit"
+            onClick = {handleLogout}
+            ><FiPower size = {26} color = "#c4c4c4"/></button> : "" }
         </div>
     );
 }
